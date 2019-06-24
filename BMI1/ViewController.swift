cbe25c27f89d3dee901cf7c3d4d4e637ff50e0de
Celine Duflo <cduflo@hotmail.com>
2019-06-24 20:39:11 +0200

    1st commit

--- BMI1/ViewController.swift
+++ BMI1/ViewController.swift
@@ -10,14 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var poids: UILabel!
+    @IBOutlet weak var taille: UILabel!
+    @IBOutlet weak var sliderPoidsStatus: UISlider!
+    @IBOutlet weak var sliderTailleStatus: UISlider!
+    @IBOutlet weak var bmi: UILabel!
+    @IBOutlet weak var message: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderPoidsStatus.value = 77.0
+        sliderTailleStatus.value = 1.70
+        updateBMI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sliderPoids(_ sender: Any) {
+        updatePoids()
+        updateBMI()
+    }
+    @IBAction func sliderTaille(_ sender: Any) {
+        updateTaille()
+        updateBMI()
+    }
+
+    func updatePoids() {
+        poids.text = "\(sliderPoidsStatus.value)"
+    }
+    func updateTaille() {
+        taille.text = "\(sliderTailleStatus.value)"
+    }
+    
+    func updateBMI() {
+        let t = sliderTailleStatus.value
+        let p = sliderPoidsStatus.value
+        
+        bmi.text = "\(p/(t*t))"
+        let bmiVal = p/(t*t)
+        updateMessage(bmiVal: bmiVal)
+    }
+    func updateMessage(bmiVal: Float) {
+
+        switch bmiVal {
+        case 0..<18.5:
+            message.text = "trop maigre"
+            message.backgroundColor = UIColor.gray
+        case 18.5..<25 :
+            message.text = "ok"
+            message.backgroundColor = UIColor.green
+        case 25..<30 :
+            message.text = "trop gros"
+            message.backgroundColor = UIColor.yellow
+        case 30..<35 :
+            message.text = "obesité severe"
+            message.backgroundColor = UIColor.orange
+        default:
+            message.text = "obesité morbide"
+            message.backgroundColor = UIColor.red
+        }
     }
 
 
